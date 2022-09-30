fae0ec8e71128e7c9548c1e3b0ea2e9eec44d1dd
Seungheon <hss980903@gmail.com>
2022-09-30 19:56:02 +0900

    풀이완료

--- implementation/문자열 재정렬.py
+++ implementation/문자열 재정렬.py
@@ -3,14 +3,16 @@ cArr = []
 nSum = 0
 
 for i in s:
-  if i.isalpha():
+  if i.isalpha(): # 알파벳인 경우
     cArr.append(i)
-  else:
+  else: # 숫자인 경우
     nSum += int(i)
 
 cArr.sort()
 
+# 숫자가 있을때만 뒤에 삽입
 if nSum != 0:
   cArr.append(str(nSum))
 
+# join 사용하면 리스트를 문자열로 변환 가능
 print(''.join(cArr))
